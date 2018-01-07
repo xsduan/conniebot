af4c172d50feb0cbd6bb6e6e6736775d27ff3d79
Shane Duan <s_duan@u.pacific.edu>
2018-01-06 17:58:10 -0800

    Add processed log message for x2i

--- bot.js
+++ bot.js
@@ -81,6 +81,8 @@ const parse = function (message) {
             }
         }, false)).then(() => logMessage('success:x2i/all'))
             .catch(err => err => logMessage('error:x2i/partial', err));
+
+        logMessage('processed:x2i');
     }
 }
 
