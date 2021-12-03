e13cff75848b9d5f780855e92b9d9444bd3404c5
William Baker <bbrk24@gmail.com>
2021-12-03 12:29:06 -0500

    Compat data check; use Date.now() in startup

--- src/helper/startup.ts
+++ src/helper/startup.ts
@@ -18,7 +18,7 @@ export async function notifyRestart(bot: Client, db: ConniebotDatabase) {
   }
 
   try {
-    const timestamp = Math.round(new Date().getTime() / 1000);
+    const timestamp = Math.round(Date.now() / 1000);
     await channel.send(`Rebooted at <t:${timestamp}:D> <t:${timestamp}:T>.`);
     log("info", `Notified ${channel} (#${channel.name || "??"}) of restart.`);
   } catch (err) {
